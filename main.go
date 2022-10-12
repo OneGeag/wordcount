177bf67e028c376789d22b1e4de83a904f80e750
George <fed.georg @ yandex dot ru>
2022-10-12 14:30:53 +0300

    rethink some logic

--- main.go
+++ main.go
@@ -9,18 +9,11 @@ import (
 func main() {
 
 	if len(os.Args) < 2 {
-		fmt.Println("Need only one argument")
+		fmt.Println("Usage: wordcount PHRASE")
 		os.Exit(1)
 	}
 
 	words := strings.Fields(os.Args[1])
 
-	var num int
-	for _, word := range words {
-		if len(word) > 0 {
-			num++
-		}
-	}
-
-	fmt.Println(num)
+	fmt.Println(len(words))
 }
